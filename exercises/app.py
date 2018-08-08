fbeb6196f6d8e33b9ad44deb06bff7f7d8ac069e
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:29:05 +0300

    :D

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return(render_template("index.html"))
+#def home_page():
+#    return(render_template("index.html"))
+
+def hello_world():
+    players = ["reckless","faker","sOAZ"] 
+    return render_template("index.html",players=players,
+    liked_players = True)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
